36689ec25c57d63edbdecd886af6d944f40332e8
hamidrehman799 <86568465+hamidrehman799@users.noreply.github.com>
2021-12-06 16:14:44 +0500

    ok

---
+++ thexmovers/android/app/src/main/kotlin/com/example/thexmovers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.thexmovers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ thexmovers/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+            ],
+          ),
+        ),
+        // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
