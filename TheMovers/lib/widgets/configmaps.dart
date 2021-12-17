81c2e1a2002aae5336e1d517b91d3a03fbfd1c2a
Hamid Rehman <86568465+hamidrehman799@users.noreply.github.com>
2021-12-17 18:57:37 +0500

    remove project from here

--- TheMovers/lib/widgets/configmaps.dart
+++
@@ -1,4 +0,0 @@
-String mp4 ='pfFLdA';
-String mp1='AIzaSyCQD';
-String mp2='gzI35xGfdeN1';
-String mp3='xVDlFT-a9KVg';
--- themovers/android/app/src/main/kotlin/com/example/themovers/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.themovers
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
